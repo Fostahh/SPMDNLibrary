d6ddbef03d9acd8454b747adad9c350f7a8cc552
Mohammad Azri Khairuddin <mohammad.azri@visionet.co.id>
2026-06-12 02:19:29 +0700

    Release 1.4.0

--- Package.swift
+++ Package.swift
@@ -7,16 +7,13 @@ let package = Package(
     name: "DNLibrary",
     platforms: [.iOS(.v15)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "DNLibrary",
-            targets: ["DNLibrary"])
+        .library(name: "DNLibrary", targets: ["DNLibrary"])
     ],
     targets: [
         .binaryTarget(
             name: "DNLibrary",
-            url: "https://github.com/Fostahh/SPMDNLibrary/releases/download/1.3.0/DNLibrary.zip",
-            checksum: "aecdc8aa0d233213d1b9ed09e897bad975cd439ffdd737e561251aa9ce1afcbd"
+            url: "https://github.com/Fostahh/SPMDNLibrary/releases/download/1.4.0/DNLibrary.zip",
+            checksum: "efa48569960a7328e3cac72c70b020a58e063401ab96320ad47a4db462a8f82c"
         )
     ]
 )
