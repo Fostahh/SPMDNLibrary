565e48c34685de3267d2fefacb546eeb1a020460
Mohammad Azri Khairuddin <mohammad.azri@visionet.co.id>
2026-08-09 09:43:23 +0700

    Release 0.7.0
    Built from Fostahh/DNLibrary@ef05ad6e8434cf36bb358c1f45147ae517a10eca (branch: development)

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DNLibrary",
-            url: "https://github.com/Fostahh/SPMDNLibrary/releases/download/0.6.0/DNLibrary.zip",
-            checksum: "0a9fa0326c8f7e1fbb9488f40f03d35f641fe224028ac89fa66f3ad1dfd87254"
+            url: "https://github.com/Fostahh/SPMDNLibrary/releases/download/0.7.0/DNLibrary.zip",
+            checksum: "97bcb3fd5560936f96a3779d196f571d802056911aced844c67181723045437e"
         )
     ]
 )
